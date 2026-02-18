0afe1647696d038ab23d5f7085a13c824aca497b
saiyeshwin <saiyeshwin@gmail.com>
2026-02-18 14:44:09 +0530

    Coded to print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/** 
+ * UC1 OOPS Banner Display Application
+ * Displays OOPS to the console
+* @author developer
+* @version 1.0
+**/
+import java.util.*;
+public class OOPSBannerApp{
+	public static void main(String[] args){
+		System.out.print("OOPS");
+}
+}
