93ed87fbe32c4df80c91968cab7c9cb3b6384322
saiyeshwin <saiyeshwin@gmail.com>
2026-02-18 15:17:25 +0530

    Coded UC5 render OOPS as banner using asterisks and array initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,21 @@
-//OOPS Banner App Use Case 4
-//This usecase improves upon the UC3 by using a string array to store the lines of the banner
-// We iterate them using for each instead of hard coding print statements
+//OOPS Banner App Use Case 5
+//This usecase extends the UC4 by defining and populating a string array in a more concise manner to store the lines of the banner
+//This enhances the readability and maintainability
 // @author Developer
-// @version 4.0
+// @version 5.0
 public class OOPSBannerArray{
 	public static void main(String[] args){
-		String[] arr=new String[7];
-		arr[0]=String.join(" ","   ***   ","   ***   ","******   ","   *****");
-		arr[1]=String.join(" "," **   ** "," **   ** ","**    ** "," **     ");
-		arr[2]=String.join(" ","**     **","**     **","**     **","**      ");
-		arr[3]=String.join(" ","**     **","**     **","**    ** ","  ****  ");
-		arr[4]=String.join(" ","**     **","**     **","******   ","     ** ");
-		arr[5]=String.join(" "," **   ** "," **   ** ","**       ","**    **");
-		arr[6]=String.join(" ","   ***   ","   ***   ","**       ","  ****  ");
-		for(String line:arr){
+		//Each line represents  a row in banner to hold letters O,O,P,S
+		String[] lines={
+			String.join(" ","   ***   ","   ***   ","******   ","   *****"),
+			String.join(" "," **   ** "," **   ** ","**    ** "," **     "),
+			String.join(" ","**     **","**     **","**     **","**      "),
+			String.join(" ","**     **","**     **","**    ** ","  ****  "),
+			String.join(" ","**     **","**     **","******   ","     ** "),
+			String.join(" "," **   ** "," **   ** ","**       ","**    **"),
+			String.join(" ","   ***   ","   ***   ","**       ","  ****  ");
+		}
+		for(String line:lines){
 			System.out.println(line);
 		}
 	}
